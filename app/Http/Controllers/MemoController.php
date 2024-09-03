ac34b584cb144e2f6fad9921c6e2fe69f14f34ef
Hiraya8823 <hirayama82007@yahoo.co.jp>
2024-09-03 11:54:43 +0900

    first commit

---
+++ app/Http/Controllers/MemoController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers;
+
+// Memoクラスを読み込む
+use App\Models\Memo;
+use App\Http\Requests\MemoRequest;
+
+class MemoController extends Controller
+{
+    // indexページへ移動
+    public function index()
+    {
+        // モデル名::テーブル全件取得
+        $memos = Memo::all();
+        // memosディレクトリーの中のindexページを指定し、memosの連想配列を代入
+        return view('memos.index', ['memos' => $memos]);
+    }
+
+    public function create()
+    {
+        return view('memos.create');
+    }
+
+    public function store(MemoRequest $request)
+    {
+        // インスタンスの作成
+        $memo = new Memo;
+
+        // 値の用意
+        $memo->title = $request->title;
+        $memo->body = $request->body;
+
+        // インスタンスに値を設定して保存
+        $memo->save();
+
+        // 登録したらindexに戻る
+        return redirect('/memos');
+    }
+
+    // showページへ移動
+    public function show($id)
+    {
+        $memo = Memo::find($id);
+        return view('memos.show', ['memo' => $memo]);
+    }
+
+    public function edit($id)
+    {
+        $memo = Memo::find($id);
+        return view('memos.edit', ['memo' => $memo]);
+    }
+
+    public function update(MemoRequest $request, $id)
+    {
+        // ここはidで探して持ってくる以外はstoreと同じ
+        $memo = Memo::find($id);
+
+        // 値の用意
+        $memo->title = $request->title;
+        $memo->body = $request->body;
+
+        // 保存
+        $memo->save();
+
+        // 登録したらindexに戻る
+        return redirect('/memos');
+    }
+    
+    public function destroy($id)
+    {
+        $memo = Memo::find($id);
+        $memo->delete();
+
+        return redirect('/memos');
+    }
+}
---
+++ resources/views/memos/edit.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>memo edit</title>
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+</head>
+<body>
+    <a href="{{ route('memos.show', $memo) }}">戻る</a>
+    <h1>更新</h1>
+
+    @if ($errors->any())
+        <div class="error">
+            <p>
+                <b>{{ count($errors) }}件のエラーがあります。</b>
+            </p>
+            <ul>
+                @foreach($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
+    <!-- 更新先はmemosのidにしないと増える sail artisan rote:listで確認① -->
+    <form action="{{ route('memos.update', $memo) }}" method="post">
+        @csrf
+        @method('PATCH')
+        <p>
+            <label for="title">タイトル</label><br>
+            <input type="text" name="title" id="title" value="{{ old('title', $memo->title) }}">
+        </p>
+        <p>
+            <label for="body">本文</label><br>
+            <textarea name="body" class="body" id="body">{{ old('body', $memo->body) }}</textarea>
+        </p>
+
+        <input type="submit" value="更新">
+    </form>
+</body>
+</html>
